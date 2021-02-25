9a20cf76420166fddc26860c5667c43931439714
lampperson <jamesevora367@gmail.com>
2021-02-25 13:12:06 -0800

    commit_2/25/2021
    adding a font and testing it out

--- Sources/Project.hx
+++ Sources/Project.hx
@@ -1,10 +1,15 @@
+import kha.Assets;
+import kha.Font;
 import kha.Framebuffer;
 
 class Project {
+
+	private var font: Font;
+
  	public function new() {}
 
 	public function init() {
-		
+		font = Assets.fonts.get("RobotoMono");
 	}
 	
 	public function update() {
@@ -12,6 +17,13 @@ class Project {
 	}
 
 	public function render(frames: Array<Framebuffer>) {
-		
+		if(frames.length > 0) {
+			var graphics = frames[0].g2;
+			graphics.begin();
+			graphics.font = font;
+			graphics.fontSize = 24;
+			graphics.drawString("Hello World", 60, 60);
+			graphics.end();
+		}
 	}
 }
